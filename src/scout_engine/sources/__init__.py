9214f4a1249784f0c9bcb294020a178d634eed75
Rahul Sharma <rahulsharma.hps@gmail.com>
2026-09-19 10:23:00 +0530

    v2: refine src/scout_engine/sources/__init__.py

--- src/scout_engine/sources/__init__.py
+++ src/scout_engine/sources/__init__.py
@@ -1,5 +1,6 @@
 from .ashby import AshbyAdapter
 from .greenhouse import GreenhouseAdapter
 from .lever import LeverAdapter
+from .workable import WorkableAdapter
 
-__all__ = ["AshbyAdapter", "GreenhouseAdapter", "LeverAdapter"]
+__all__ = ["AshbyAdapter", "GreenhouseAdapter", "LeverAdapter", "WorkableAdapter"]
